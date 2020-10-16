0f04f797a6402d41e912710b9f42a5b19767c47f
Olugbemi Samuel <olugbemisamuel600@gmail.com>
2020-10-16 10:36:22 +0100

    First commit

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -8,14 +8,14 @@ namespace Task7
         {
             Console.Write("Enter Hourly Rate: ");
             double hourlyRate = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter Work Hour: ");
+            Console.Write("Enter Number of Hours Worked: ");
             int workHour = Convert.ToInt32(Console.ReadLine());
             decimal grossPay = 0;
-            int laterHour = workHour - 40;
-            decimal extraPay = 1.5M * laterHour;
             if(workHour <= 40){
                 grossPay = (decimal)hourlyRate * (decimal)workHour;
             } else if(workHour > 40){
+                int laterHour = workHour - 40;
+                decimal extraPay = 1.5M * laterHour;
                 grossPay = ((decimal)hourlyRate * 40M) + ((decimal)hourlyRate * extraPay);
             }
             Console.WriteLine($"Pay for employee is {grossPay:C}");
